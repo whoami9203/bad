a540b45efc5a6cf3c02281cc70dca27c200a714b
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2023-05-17 16:33:07 +0800

    Update talk.py

--- talk.py
+++ talk.py
@@ -17,11 +17,12 @@ number_str = "0123456789"
 # Iterate over each combination
 for x1 in range(10):
     print("x1 = ", x1)
-    
+
     for x2 in range(10):
         for x3 in range(10):
             for x4 in range(10):
                 cmdId = number_str[x1] + number_str[x2] + number_str[x3] + number_str[x4]   # Convert to bytes
+                print("\n", cmdId)
                 connect_to_c2_agent(cmdId, b"some_data")
 
                 # Sleep to add delay between connections (adjust as needed)
