6a158109cf249c1aef3e5ec50abb87425819e556
whoami9203 <39827915+whoami9203@users.noreply.github.com>
2023-05-17 17:35:31 +0800

    Update talk.py

--- talk.py
+++ talk.py
@@ -3,28 +3,23 @@ import time
 
 
 def connect_to_c2_agent(cmdId, data):
-    print("Trying cmdId:", cmdId.decode())
+    print("cmdId: ", cmdId.decode())
     io = remote("127.0.0.1", 7373)
     header = b'nslab_w31c0m3_U\x00'
     cmd = cmdId + b'\x00' * ( 4 - len(cmdId) )
     content = header + cmd + data
     io.send(content)
-    time.sleep(0.001)
+    time.sleep(0.000001)
     io.close()
 
 
 
 number_str = "0123456789"
 
-# Iterate over each combination
 for x1 in range(10):
-    print("x1 = ", x1)
-
     for x2 in range(10):
         for x3 in range(10):
             for x4 in range(10):
                 cmdId = str.encode(number_str[x1] + number_str[x2] + number_str[x3] + number_str[x4])   # Convert to bytes
-                connect_to_c2_agent(cmdId, b"some_data")
-
-                # Sleep to add delay between connections (adjust as needed)
-                time.sleep(0.001)
+                connect_to_c2_agent(cmdId, b"data")
+                time.sleep(0.000001)
